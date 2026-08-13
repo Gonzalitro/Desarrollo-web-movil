f218ebce16b325a79eca487d8cf96512cae3c973
Gonzalitro <maximo.gonzalez.ye@gmail.com>
2026-08-13 11:11:09 -0400

    Agregando archivos de la primera pagina web

---
+++ pagwebprimera/contacto.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Paguina Contacto </title>
+    </head>
+    <body>
+        Pagina Contacto
+        <a href="index.php"> ir a Principal </a><br>
+        <a href="empresa.php"> ir a Empresa </a><br>
+        <a href="producto.php"> ir a Producto </a><br>
+        <a href="servicio.php"> ir a Servicio </a><br>
+        <a href="contacto.php"> ir a Contacto </a>
+    </body>
+</html>

d8628ec5f33cfba6c44ead0b684cfd2c6aba5b11
Gonzalitro <maximo.gonzalez.ye@gmail.com>
2026-08-14 18:22:19 -0400

    Se termino lo visto en clases del miercoles 12/08 haciendo todo lo visto por el profe

--- pagwebprimera/empresa.php
+++ pagwebprimera/empresa.php
@@ -1,13 +1,103 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
     <head>
-        <title>Paguina Empresa </title>
+        <title>Paguina Principal </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        Pagina Empresa
-        <a href="index.php"> ir a Principal </a><br>
-        <a href="empresa.php"> ir a Empresa </a><br>
-        <a href="producto.php"> ir a Producto </a><br>
-        <a href="servicio.php"> ir a Servicio </a><br>
-        <a href="contacto.php"> ir a Contacto </a>
+        <!-- navbar -->    
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Logo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Empresa</a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Nuestra Empresa</a></li>
+            <li><a class="dropdown-item" href="#">Nuestro equipo</a></li>
+            <li><a class="dropdown-item" href="#">Mision</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Productos.php">Productos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Servicios.php">Servicios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Contactos.php">Contactos</a>
+        </li>  
+      </ul>
+        </div>
+        <button type="button" class="btn btn-outline-primary "data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button>
+            </div>
+        </nav>
+    
+        <!-- container -->
+        <div class="container-fluid bg-warning">
+            Pagina Empresa<br>
+            <a href="index.php"> ir a Principal </a><br>
+            <a href="Empresa.php"> ir a Empresa </a><br>
+            <a href="Productos.php"> ir a Productos </a><br>
+            <a href="Servicios.php"> ir a Servicios </a><br>
+            <a href="Contactos.php"> ir a Contactos </a>
+        </div>
+
+        <!-- footer -->
+        <div class="container-fluid bg-dark">
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-4 d-flex justify-content-center"
+                style="color:white"><strong>Miempresa@hotmail.com</strong></div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+         <!-- modal -->
+          <div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+            <h4 class="modal-title">Autenticar</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        <form action="Empresa.php">
+        <div class="mb-2 mt-2">
+            <label for="email" class="form-label">Email:</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+        </div>
+        <div class="mb-2">
+            <label for="pwd" class="form-label">Password:</label>
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+        </div>
+        <div class="form-check mb-2">
+            <label class="form-check-label">
+            <input class="form-check-input" type="checkbox" name="remember"> Remember me
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Login <i class="fa fa-check-circle"></i></button></button>
+        </form>
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
     </body>
 </html>
